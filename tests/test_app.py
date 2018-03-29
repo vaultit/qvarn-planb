f0224a5d4b6fbaad00dc6d62af04e160e03895da
Mantas <sirexas@gmail.com>
2018-03-29 08:57:25 +0300

    Add a test case with duplicate field names

--- tests/test_app.py
+++ tests/test_app.py
@@ -39,3 +39,31 @@ def test_create_get_list(client):
     rows = client.get('/contracts').json()
     ids = {x['id'] for x in rows['resources']}
     assert row['id'] in ids
+
+
+def test_duplicate_names_case(client):
+    data = {
+        'type': 'org',
+        'country': 'fi',
+        'names': ['Orgtra'],
+        'gov_org_ids': [
+            {
+                'country': 'lt',
+                'org_id_type': 'testid',
+                'gov_org_id': 'ORG-0001',
+            },
+        ],
+        'contacts': [
+            {
+                'contact_type': 'home',
+                'contact_roles': ['role'],
+                'country': 'ua',
+            }
+        ]
+    }
+
+    row = client.post('/orgs', json=data).json()
+    assert row == dict(data, id=row['id'], revision=row['revision'])
+
+    row = client.get('/orgs/' + row['id']).json()
+    assert row == dict(data, id=row['id'], revision=row['revision'])
